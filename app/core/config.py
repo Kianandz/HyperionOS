a9e665f5ba50a1139184939f7cac4342374f3443
kianandz <nandaa.me64@gmail.com>
2026-09-06 00:48:59 +0700

    fix port

--- app/core/config.py
+++ app/core/config.py
@@ -4,6 +4,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     APP_NAME: str = os.getenv("TITLE", "HyperionOS")
     VERSION: str = os.getenv("VERSION", "1.0.0")
+    PORT: str = os.getenv("PORT")
     SECRET_KEY: str = os.getenv("SECRET_KEY", "hyperion-secret-key-change-this")
     DEBUG: bool = True
 
